72c408df5abd6d7a7cea888032328c28f6f20664
mj1999 <mayank.joshi92@yahoo.com>
2023-09-09 23:42:07 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require("../controller/userController");
 const passport = require("passport");
 
-router.post("/create", passport.checkAuth, userController.createUser);
+router.post("/create", userController.createUser);
 router.get("/view", passport.checkAuth, userController.employeeView);
 router.post("/toggle-status", passport.checkAuth, userController.toggleStatus);
 router.post("/remove", passport.checkAuth, userController.remove);
